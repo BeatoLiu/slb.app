600402b0c3758569a6a21b6818a78956f9852a8a
BeatoLiu <39610167+BeatoLiu@users.noreply.github.com>
2022-06-27 10:03:31 +0800

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -7,10 +7,10 @@ const getHostName = () => {
 			hostName = '/api' // 这里是本地的请求url
 			break
 		case 'test': // 注意这里的名字要和设置的模式名字对应起来
-			hostName = 'http://tk.2qzs.com' // 这里是测试环境中的url
+			hostName = '' // 这里是测试环境中的url
 			break
 		case 'production':
-			hostName = 'http://tk.2qzs.com' // 生产环境url
+			hostName = '' // 生产环境url
 			break
 	}
 	return hostName
@@ -24,10 +24,10 @@ const getPayName = () => {
 			hostName = '/pay' // 这里是本地的请求url
 			break
 		case 'test': // 注意这里的名字要和设置的模式名字对应起来
-			hostName = 'http://slpayservice.2qzs.com' // 生产环境url
+			hostName = '' // 生产环境url
 			break
 		case 'production':
-			hostName = 'http://slpayservice.2qzs.com' // 生产环境url
+			hostName = '' // 生产环境url
 			break
 	}
 	return hostName
@@ -37,15 +37,15 @@ const getPicName = () => {
 	let picName
 	switch (process.env.VUE_APP_ENV) {
 		case 'development':
-			picName = 'https://ytxxkj-bucket.oss-cn-hangzhou.aliyuncs.com/uploadTest/' // 这里是本地的请求url
-			// picName = 'https://ytxxkj-bucket.oss-cn-hangzhou.aliyuncs.com/upload/' // 生产环境url
+			picName = '' // 这里是本地的请求url
+			// picName = '' // 生产环境url
 			break
 		case 'test': // 注意这里的名字要和设置的模式名字对应起来
-			// picName = 'https://ytxxkj-bucket.oss-cn-hangzhou.aliyuncs.com/uploadTest/' // 这里是测试环境中的url
-			picName = 'https://ytxxkj-bucket.oss-cn-hangzhou.aliyuncs.com/upload/' // 生产环境url
+			// picName = '' // 这里是测试环境中的url
+			picName = '' // 生产环境url
 			break
 		case 'production':
-			picName = 'https://ytxxkj-bucket.oss-cn-hangzhou.aliyuncs.com/upload/' // 生产环境url
+			picName = '' // 生产环境url
 			break
 	}
 	return picName
@@ -56,9 +56,9 @@ const payName = getPayName()
 const picName = getPicName()
 // const picDisplayPath = getPicPath()
 const locationOrigin = window.location.origin
-const assetsOrigin = 'http://mg.2qzs.com'
-const payOrigin = 'http://slpay.2qzs.com'
-const jdImgPath = 'http://img13.360buyimg.com/n1/'
+const assetsOrigin = ''
+const payOrigin = ''
+const jdImgPath = ''
 
 export {
 	// 接口地址
